cc270650c7e96bf2c39134e3b87fffd92a5edeeb
adwxithc <adwaithjanardhanan0@gmail.com>
2024-08-12 18:42:30 +0530

    update: updated backend access in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+dotenv.config();
+
+// Access the environment variable
+const backendUrl = process.env.VITE_BACKEND_URL;
+
 
 
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    proxy:{
-      '/api':{ 
-        target:'',
-        changeOrigin:true,
-        secure:true
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: backendUrl,
+        changeOrigin: true,
+        secure: true
       }
-    } 
+    }
   }
 })
